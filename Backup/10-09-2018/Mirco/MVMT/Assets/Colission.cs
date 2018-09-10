ba289dcf8191d8fe07678e9ae8d82e1abd291d29
tomhaupt <tom-haupt@gmx.de>
2018-09-10 19:35:55 +0200

    Added Backup
    Added Backup

---
+++ Backup/10-09-2018/Mirco/MVMT/Assets/Colission.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Colission : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            //Destroy(col.gameObject);
+           // forwardForce = 700f;
+        }
+    }
+}
---
+++ Backup/10-09-2018/Mirco/MVMT/Assets/MVMT.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+public class MVMT : MonoBehaviour {
+
+    public Rigidbody rb; // reference to the Rigidbody called "rb"
+    public float forwardForce = 700f; //Speed of the ForwardForce
+    public float sidewaysForce = 500f; //Speed of the SidewaysForce
+
+    // Update is called once per frame 
+    void FixedUpdate () {
+
+        //add forward and backward Force
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forwardForce * Time.deltaTime);
+        }
+
+       
+        //add SidewayForce
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
+        }
+	}
+}
