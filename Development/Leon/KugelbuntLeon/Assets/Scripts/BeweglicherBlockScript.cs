0c33e02c809961607e3425af93670667c4fee392
DESKTOP-U68V3RC\Leon <leonschillings139@gmail.com>
2018-09-13 03:55:49 +0200

    Szene erstellt mit einer SchalterPlane und einer zugehörigen Wand. Rollt man mit der Kugel auf die Plane dann bewegt sich die Wand einen festen Wert nach oben und macht den Weg frei. Noch nicht getestet aber ich glaube jeder Rigidbody löst den Collider der Plane aus, nicht nur der Player.

---
+++ Development/Leon/KugelbuntLeon/Assets/Scripts/BeweglicherBlockScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeweglicherBlockScript : MonoBehaviour {
+
+
+    
+    
+    public bool schalter;  // damit das Objekt nicht sofort aufsteigt
+    private float zielHoehe;
+
+	void Start () {
+
+        zielHoehe = 10; //damit das Objekt an einer bestimmten Höhe stehen bleibt
+         schalter = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (schalter == true && this.transform.position.y <= zielHoehe)  //bewegt das Objekt
+        { transform.Translate(0, Time.deltaTime, 0); }
+    }
+
+   public void schalterUmlegen() // diese Funktion wird von dem anderen Objekt aufgerufen wenn dort der Collider berührt wird
+    {
+        schalter = true;
+    }
+
+}
