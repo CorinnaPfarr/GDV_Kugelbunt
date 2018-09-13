ee7123abaa82cdaac68fac383d12acc22b511d5c
Mirco Schlichting <digitalmediamirco@gmail.com>
2018-09-13 21:44:20 +0200

    Sprung- + Speedplattform
    Nach einigen Git Problemen jetzt GitHub statt Sourcetree benutzt. Sprungplattform, Movement/Speed geupdated, Speedplattform gecodet.

---
+++ Development/Mirco/MVMT/Assets/Scripts/Jump.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour {
+    [SerializeField] private float _jumpFactor = 30;
+    public GameObject player;
+
+    protected void OnCollisionEnter(Collision collision)
+    {
+        GameObject collingObject = collision.gameObject;
+        if (collingObject.gameObject == player)
+        {
+            player.GetComponent<Rigidbody>().AddForce(Vector3.up * _jumpFactor, ForceMode.Impulse);
+        }
+    }
+}
