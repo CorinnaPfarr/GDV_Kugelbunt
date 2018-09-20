65b40a8dbd6e254fee92ca871eb148cddc18faec
DESKTOP-U68V3RC\Leon <leonschillings139@gmail.com>
2018-09-20 06:29:22 +0200

    Kanone+Projektil hinzugefügt. Kanone dreht sich zum Spieler und schießt in regelmäßigen Abständen Kugeln auf den Spieler die sich nach X Sekunden selbst zerstören. Desweiteren alte und neue Objekte als Prefab gespeichert

---
+++ Development/Leon/KugelbuntLeon/Assets/Scripts/KanoneScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KanoneScript : MonoBehaviour {
+
+
+    private bool inReichweite =false;
+    private  GameObject player;
+    private float intervall = 3;
+    public GameObject bullet;
+    public float ticktack;
+    
+
+
+    void Start () {
+     player = GameObject.Find("Player");
+     //bullet = GameObject.Find("Projektil");
+ 
+        ticktack = intervall;
+
+        
+    }
+
+	
+	
+	void Update () {
+
+        Vector3 playerPostition = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z); //hier wird sichergelegt, dass sich die Kanone nur auf der Y Achse dreht
+
+        if (inReichweite == true)
+        {
+            transform.LookAt(playerPostition);
+
+           
+            ticktack -= Time.deltaTime;
+            if (ticktack <= 0f)
+            {
+                print("peng");
+               // GameObject bullet;
+                Instantiate(bullet, transform.position + (player.transform.position - transform.position).normalized, transform.rotation); //das Projektil Prefab wird Instanziert und zwischen Spieler und Objekt platziert
+                ticktack = intervall;
+            }
+        }
+
+	}
+    
+    void OnTriggerEnter()
+    {
+        inReichweite = true;
+    }
+
+    void OnTriggerExit()
+    {
+        inReichweite = false;
+    }
+}
---
+++ Development/Leon/KugelbuntLeon/Assets/Scripts/ProjektilSkript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjektilSkript : MonoBehaviour {
+
+    public float selbstzerstörung = 5;
+    public float speed = 10;
+
+
+	void Start () {
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        selbstzerstörung -= Time.deltaTime;
+        transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.Self);
+        if (selbstzerstörung <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
