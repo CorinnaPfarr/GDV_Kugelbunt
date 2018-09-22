dd5a908c87a8bb33575c22a98442f3af60365f43
tomhaupt <tom-haupt@gmx.de>
2018-09-23 00:49:27 +0200

    deleted old folder
    deleted development folder

--- Development/Tom/GDV_Kugelbunt_Tom_Haupt/GDV_Kugelbunt_TH/Assets/Scripts/deprecated/WayColliderScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class WayColliderScript : MonoBehaviour {
-
-	// Collision Function
-	void OnCollisionEnter(Collision colObj) {
-		Debug.Log(colObj.collider.name + " hited!");
-	}
-}
