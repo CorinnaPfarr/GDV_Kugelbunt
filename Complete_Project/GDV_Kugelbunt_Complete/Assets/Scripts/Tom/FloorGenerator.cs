150a4d963583474d9cf2d085ff66d07dc3cec704
tomhaupt <tom-haupt@gmx.de>
2018-09-23 19:38:13 +0200

    added isTrigger again
    - isTrigger lines of code got deleted
    thank you git <3

--- Complete_Project/GDV_Kugelbunt_Complete/Assets/Scripts/Tom/FloorGenerator.cs
+++ Complete_Project/GDV_Kugelbunt_Complete/Assets/Scripts/Tom/FloorGenerator.cs
@@ -128,10 +128,20 @@ public class FloorGenerator : MonoBehaviour {
 		endCube.GetComponent<Renderer>().material = floorMat;
 		endCubeRotator.GetComponent<Renderer>().material = floorMatEnd;
 
+		// Set isTrigger
+		// Assign Script to endCubeRotator
+		endCubeRotator.AddComponent<EndScript>();
+		// Get the Collider of the endCubeRotator
+		Collider endCubeRotatorCol = endCubeRotator.GetComponent<Collider>();
+		// set is Trigger
+		endCubeRotatorCol.isTrigger = true;
+
 		// Assign Script to endCubeRotator
 		endCubeRotator.AddComponent<EndScript>();
 		// Add Tag for sound
 		endCubeRotator.transform.gameObject.tag = "Ziel";
+
+		// Set isTrigger
 
 		// Set StartCube, endCube Size
 		startCube.transform.localScale = new Vector3(8.0f, 0.5f, 8.0f);
